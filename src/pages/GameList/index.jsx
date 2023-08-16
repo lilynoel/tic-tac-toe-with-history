f765cc482aa056c6aa2a5a1e59b2f5a71c4f950a
lilynoel <lilydnas@gmail.com>
2023-08-16 20:42:35 +1000

    created create game functionality

--- src/pages/GameList/index.jsx
+++ src/pages/GameList/index.jsx
@@ -1,22 +1,35 @@
 import React, { useEffect, useState } from "react";
-import { getAllGames } from "../../utilities/kontent-utils";
+import {
+  createNewGame,
+  getAllGames,
+  nextGameNum,
+} from "../../utilities/kontent-utils";
 import { Link } from "react-router-dom";
 
 const GameList = () => {
   const [games, setGames] = useState([]);
+  const [count, setCount] = useState(0);
+  const onButtonClick = () => {
+    const nextNum = nextGameNum(games);
+    createNewGame(nextNum).then((response) => setCount(count + 1));
+  };
   useEffect(() => {
     getAllGames().then((response) => setGames(response));
-  }, []);
+  }, [count]);
   return (
     <>
       <h1> This is the game list! </h1>
-      {games.map((game) => {
-        return (
-          <Link to={`/games/${game.id}`} key={game.id}>
-            {game.name}
-          </Link>
-        );
-      })}
+      <button onClick={onButtonClick}> Create Game </button>
+      <br></br>
+      <ul>
+        {games.map((game) => {
+          return (
+            <li key={game.id}>
+              <Link to={`/games/${game.id}`}>{game.name}</Link>
+            </li>
+          );
+        })}
+      </ul>
     </>
   );
 };
--- src/utilities/kontent-utils.js
+++ src/utilities/kontent-utils.js
@@ -5,6 +5,7 @@ export async function getAllGames() {
   const games = allItems.data.items.filter(
     (item) => item.collection.id === "e3cca0c7-4e62-42fa-9d6a-222137b3a2e7"
   );
+  console.log(games);
   return games;
 }
 
@@ -46,4 +47,57 @@ function translateMovesToBoard(moves) {
     board[move.coordinate] = move.symbol;
   });
   return board;
+}
+
+export async function createNewGame(num) {
+  const response = await client
+    .addContentItem()
+    .withData({
+      name: "Game " + num,
+      codename: "game_" + num,
+      type: { codename: "game" },
+      collection: { codename: "games_collection" },
+    })
+    .toPromise();
+  const { id } = response.data;
+  const defaultState = {
+    draw: "false",
+    winner: "",
+    currentPlayer: Math.random() > 0.5 ? "X" : "O",
+  };
+  const game = await updateGameById(id, defaultState);
+  return game;
+}
+
+export function nextGameNum(games) {
+  const last = games[games.length - 1];
+  const lastNum = last.codename.split("_")[1];
+  return parseInt(lastNum) + 1;
+}
+
+export async function updateGameById(id, gameState) {
+  const response = await client
+    .upsertLanguageVariant()
+    .byItemId(id)
+    .byLanguageCodename("default")
+    .withData((builder) => {
+      return {
+        elements: [
+          builder.textElement({
+            element: { codename: "current_player" },
+            value: gameState.currentPlayer,
+          }),
+          builder.textElement({
+            element: { codename: "winner" },
+            value: gameState.winner,
+          }),
+          builder.textElement({
+            element: { codename: "draw" },
+            value: String(gameState.draw),
+          }),
+        ],
+      };
+    })
+    .toPromise();
+  return response;
 }
